e1b1652f1e4d3b6242bccf3e4a3d5f7fb91d4d46
Maxim Safaryan <safaryanmt@gmail.com>
2025-02-18 22:26:52 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import streamlit as st
-# import yfinance as yf
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from datetime import datetime
+
 
 st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWZoYWc5NzRiaGVrMGIyZjl3eXF5dXQ1ODZxZHBpdjJzd2ZtMDVnNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Oj5w7lOaR5ieNpuBhn/giphy.gif", width=500)
 
